c64177669a48b6251552e1fd5f3e8c65fb3fe906
DESKTOP-MGAC9P3\Nevil <ndabre@myeffect.ca>
2019-12-31 01:15:53 -0500

    fixed sound

--- src/components/main.js
+++ src/components/main.js
@@ -54,9 +54,9 @@ const Main = (props) => {
 			<symbol id="icon-close" viewBox="0 0 24 24">
 				<path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
 			</symbol>
-			<symbol id="icon-settings" viewBox="0 0 24 24">
+			{/* <symbol id="icon-settings" viewBox="0 0 24 24">
 				<path d="M19.43 12.98c.04-.32.07-.64.07-.98s-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.12-.22-.39-.3-.61-.22l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65C14.46 2.18 14.25 2 14 2h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.23-.09-.49 0-.61.22l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98s.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.12.22.39.3.61.22l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.23.09.49 0 .61-.22l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.65zM12 15.5c-1.93 0-3.5-1.57-3.5-3.5s1.57-3.5 3.5-3.5 3.5 1.57 3.5 3.5-1.57 3.5-3.5 3.5z" />
-			</symbol>
+			</symbol> */}
 			<symbol id="icon-sound-on" viewBox="0 0 24 24">
 				<path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z" />
 			</symbol>
@@ -170,7 +170,7 @@ const Main = (props) => {
 	<img alt="city" src="https://webstockreview.net/images/city-clipart-metropolitan-area-12.png" className="city-image" />
 	<div className="bottom-panel">
 	
-		<div className="searchbar" ref={searchBoxRef}>
+		<div className="searchbar animated rubberBand slow delay-5s" ref={searchBoxRef}>
 		<form ref={formRef} onSubmit={e => e.preventDefault()}>
 		<input
           type="text"
@@ -185,9 +185,9 @@ const Main = (props) => {
 		</div>
 		
 
-		<div className="whatsappbar hide" ref={whatsappBoxRef}>
+		<div className="whatsappbar hide animated infinite bounce slow delay-5s" ref={whatsappBoxRef}>
 			<a href={`whatsapp://send?text=A special 🎁 message from *${name}*, See here(No Spam)➡️ ${process.env.REACT_APP_DOMAIN}/publish?clientname=${name}`} className="whatsapp-btn">
-				<h2><i className="fab fa-whatsapp"></i> Share on Whatsapp <i className="fab fa-whatsapp"></i></h2>
+				<h2><i className="fab fa-whatsapp"></i>Share on Whatsapp<i className="fab fa-whatsapp"></i></h2>
       		</a>
 		</div>
 	</div>
--- src/components/waveText/WaveText.js
+++ src/components/waveText/WaveText.js
@@ -4,9 +4,9 @@ import { times } from 'lodash';
 
 const WaveText = (props) => {
     return (
-        <div class="wave-text-stage">
+        <div className="wave-text-stage">logo
             {
-              times(15, i => <div class="wave-text-layer" data-newline="&#xa;" data-firstline={props.greetings} data-secondline="A Happy New Year!" data-thirdline="2020"></div>)            
+              times(15, i => <div className="wave-text-layer" key={i} data-newline="&#xa;" data-firstline={props.greetings} data-secondline="A Happy New Year!" data-thirdline="2020"></div>)            
             }
         </div>
     )
