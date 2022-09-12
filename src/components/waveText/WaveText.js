aede030d30968b941808a3c3dba4d02c48801de7
Neville Dabre <nevil_dabre@yahoo.com>
2022-09-12 12:05:50 -0400

    Update WaveText.js

--- src/components/waveText/WaveText.js
+++ src/components/waveText/WaveText.js
@@ -16,7 +16,7 @@ const WaveText = (props) => {
     return (
         <div className="wave-text-stage">logo
             {
-              times(15, i => <div className="wave-text-layer" key={i} data-newline="&#xa;" data-firstline={props.greetings} data-secondline="A Happy New Year!" data-thirdline="2020"></div>)            
+              times(15, i => <div className="wave-text-layer" key={i} data-newline="&#xa;" data-firstline={props.greetings} data-secondline="A Happy New Year!" data-thirdline={year}></div>)            
             }
         </div>
     )
